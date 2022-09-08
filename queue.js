8f5ba0dd7e44840bdcbcee6dab3804cc6f77d218
parzidog <kenneth.schaefer0@gmail.com>
2022-09-08 11:48:41 -0500

    mostly finished

--- queue.js
+++ queue.js
@@ -6,18 +6,26 @@ class Queue {
   }
   enqueue(value) {
     // add an element to our object
+    `${this.element}.${value}`=value
   }
   dequeue() {
     // remove an element from our object and return the item removed
   }
   peak() {
     // return the item at the front of the queue
+    return this.element[0]
   }
   get length() {
     // return the length of the queue
+    return this.pancake.length
   }
-  get isEmpty() {}
+  get isEmpty() {
   // return a true or false value if the queue is empty
+    if (this.element=={}){
+        return true;
+      }
+    return false;
+  }
 }
 
 const myQueue = new Queue();
--- stack.js
+++ stack.js
@@ -4,16 +4,23 @@ class Stack {
   }
   push(value) {
     // add an item to the stack
+    this.pancake.push(value)
   }
   pop() {
     // remove and return an item from the stack
+    return this.pancake.pop();
   }
   isempty = () => {
     // return a true or false value if empty
+    if (this.pancake.isempty){
+      return true;
+    }
+    return false;
   };
 
   size = () => {
     // return the length of the stack
+    return this.pancake.length
   };
 }
 const myStack = new Stack();
